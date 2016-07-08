c6c22158c9c4c5b556655cf4a80d1ae0832627ca
Sasemohan <sasemohan90950@gmail.com>
2016-07-08 18:46:12 +0530

    Create strmultiply.c

---
+++ strmultiply.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void main()
+{
+char a[]="12";
+char c[]="11";
+int b,d;
+clrscr();
+b=atoi(a);
+d=atoi(c);
+printf("%d",b*d);
+getch();
+}
