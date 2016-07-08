2ffd36f815bcc891baa6b1b6ad2a82534b60c7bb
Sasemohan <sasemohan90950@gmail.com>
2016-07-08 12:47:26 +0530

    Create powoftwo.c

---
+++ powoftwo.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+double x, ret;
+int rounded;
+clrscr();
+printf("enter the value");
+scanf("%lf",&x);
+
+   /* finding log*/
+   ret = log(x);
+ double notRounded = 4.1666667
+rounded = (int)ceil(x);
+if(x==rounded)
+{
+printf("power of 2");
+}
+getch();
+}
